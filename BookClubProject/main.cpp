27c697f5296e2da431af74f5f29563aadfceadff
azalea-bushes <azin.mobasheri@gmail.com>
2026-06-28 23:32:53 +0330

    Add login page and resources

--- BookClubProject/main.cpp
+++ BookClubProject/main.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
+#include "SignWindow/loginwindow.h"
+#include "SignWindow/registerwindow.h"
+#include "SignWindow/forgotpasswordwindow.h"
+#include <QResource>
+
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    LoginWindow w1;
+    w1.show();
+
+    RegisterWindow w2;
+    w2.show();
+
+    ForgotPasswordWindow w3;
+    w3.show();
+
+    //MainWindow w;
+    //w.show();
     return QApplication::exec();
 }
