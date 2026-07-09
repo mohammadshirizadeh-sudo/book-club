e96475940258bc165606f65d2da6b75f8f37ae95
azalea-bushes <azin.mobasheri@gmail.com>
2026-07-09 21:33:47 +0330

    added new dialogs for users and publishers

--- BookClubProject/Publishers/mybooks.cpp
+++ BookClubProject/Publishers/mybooks.cpp
@@ -1,6 +1,11 @@
 #include "mybooks.h"
 #include "Publishers/ui_mybooks.h"
 
+#include "../appWindow/SessionManager.h"
+#include "../Network-Manger/NetworkManager.h"
+#include "../Server/Request.h"
+#include "../Server/Response.h"
+
 MyBooks::MyBooks(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MyBooks)
@@ -12,3 +17,39 @@ MyBooks::~MyBooks()
 {
     delete ui;
 }
+
+void MyBooks::on_addBookPushButton_clicked()
+{
+
+}
+
+
+void MyBooks::on_editBookPushButton_clicked()
+{
+
+}
+
+
+void MyBooks::on_staticsPushButton_clicked()
+{
+
+}
+
+
+void MyBooks::on_applyDiscountPushButton_clicked()
+{
+
+}
+
+
+void MyBooks::on_deleteDeactivatePushButton_clicked()
+{
+
+}
+
+
+void MyBooks::on_backPushButton_clicked()
+{
+
+}
+
